edfda4998b2d4a909ad6a95c6968f4bc15f8589c
Damianp99 <damianperez200199@gmail.com>
2022-01-13 17:03:59 +0100

    snack 1 done

--- js/snack1.js
+++ js/snack1.js
@@ -1,3 +1,18 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+
+/*
+ESERCIZIO CON FOR 
+*/
+
+let number;
+
+let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+    number = parseInt(prompt('Inserisci un numero'))
+    console.log(number)
+    sum += number
+}
+console.log(sum)
